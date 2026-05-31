57974e78622a6dcd32950dff9d922e649637266d
Frank Zhang <rgb360@126.com>
2026-05-31 18:31:32 +0800

    Sync mosdns/geodata sources and drop docker leftovers

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -3,12 +3,24 @@
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
-# Clone packages
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
 
 # Adjust packages
+
+# 移除 openwrt feeds 过时的luci版本
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+# Clone packages
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 --depth=1 clone/mosdns
+git clone https://github.com/sbwml/v2ray-geodata --depth=1 clone/v2ray-geodata
 cp -rf clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/mosdns/luci-app-mosdns feeds/luci/applications/
+cp -rf clone/v2ray-geodata feeds/packages/net/
+
+
+
+
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
